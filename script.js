cf67a668efdf603a3945740bfcdbfad1dc378262
Mickey Capper <mickey.capper@gmail.com>
2024-04-25 20:58:07 -0600

    writing code to pull all words from the p elements on a wikipedia page

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 console.log("Hello Nathan!")
 console.log("This is a popup!")
 
-const body = document.querySelector("body")
-const allText = body.textContent
+const graphs = document.querySelectorAll('p');
+
+//will contain
+const cleanWords =[];
+//cycle through graphs
+graphs.forEach( (p) =>{
+  //take the text content of the graph
+  const pText = p.textContent;
+  //assemble it into an array
+  const wordMatchRegExp = /[^\s]+/g;
+  const words = pText.matchAll(wordMatchRegExp);
+  const rawWords = [...words];
+  //gather into cleanWords
+  //console.log(rawWords);
+  rawWords.forEach((word)=>{
+    cleanWords.push(word[0])
+  })
+  }
+)
+console.log(cleanWords)
+
 
